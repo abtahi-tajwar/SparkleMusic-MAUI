b94646e6fbb68dac08b238d669b448a6c59506d6
abtahi-tajwar <abtahitajwar@gmail.com>
2025-03-06 22:18:13 -0500

    Music context menu UI implementation, file picker for mac implementation attempt but not working

--- SparkleMusic-MAUI/Services/StorageService.cs
+++ SparkleMusic-MAUI/Services/StorageService.cs
@@ -1,4 +1,5 @@
 using Microsoft.Maui.Storage;
+using Debug = System.Diagnostics.Debug;
 
 namespace SparkleMusic_MAUI.Services;
 
@@ -17,7 +18,8 @@ public class StorageService
                         DevicePlatform.WinUI, [".mp3", ".wav", ".aac", ".flac", ".m4a", ".ogg"]
                     }, // Common audio file extensions
                     { DevicePlatform.Tizen, ["audio/*"] }, // Allow all audio types
-                    { DevicePlatform.macOS, ["public.audio"] } // UTType for macOS
+                    { DevicePlatform.macOS, ["mp3", "wav", "aiff"] }, // UTType for macOS
+                    { DevicePlatform.MacCatalyst, ["public.audio"] } // UTType for macOS
                 });
 
             PickOptions options = new()
@@ -25,12 +27,19 @@ public class StorageService
                 PickerTitle = "Please select an audio file",
                 FileTypes = customFileType,
             };
+            // PickOptions options = new()
+            // {
+            //     PickerTitle = "Please select an audio file",
+            //     FileTypes = FilePickerFileType.Videos,
+            // };
+
             var result = await FilePicker.Default.PickMultipleAsync(providedOptions ?? options);
             return result;
         }
         catch (Exception ex)
         {
             // The user canceled or something went wrong
+            Debug.WriteLine(ex.Message);
         }
 
         return null;
---
+++ SparkleMusic-MAUI/Views/MainPage/MusicContextMenu.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkleMusic_MAUI.Views.MainPage;
+
+public partial class MusicContextMenu : ContentView
+{
+    public MusicContextMenu()
+    {
+        InitializeComponent();
+    }
+}
